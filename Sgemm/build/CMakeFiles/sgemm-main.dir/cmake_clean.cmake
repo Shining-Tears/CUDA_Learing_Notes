1a8502fa88baee044ddbc7447561505545b25bb1
root <root@DESKTOP-DV8R7D5>
2025-05-26 02:20:07 +0800

    sgemm double buffer

---
+++ Sgemm/build/CMakeFiles/sgemm-main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sgemm-main.dir/main.cpp.o"
+  "CMakeFiles/sgemm-main.dir/main.cpp.o.d"
+  "CMakeFiles/sgemm-main.dir/sgemm.cu.o"
+  "CMakeFiles/sgemm-main.dir/sgemm.cu.o.d"
+  "sgemm-main"
+  "sgemm-main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/sgemm-main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
